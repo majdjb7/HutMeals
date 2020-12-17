0fb06ffa995c682f237fadabfe82fe4f653be201
Majd Jaber <majdjd171@gmail.com>
2020-12-17 23:38:23 +0200

    first_commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import React, { Component } from 'react'
+import './App.css';
+
+import { Navbar, Nav, NavItem, MenuItem, NavDropdown } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className='App'>
+        <h2>"Hi"</h2>
+    </div>
+  );
+}
+
+export default App;
