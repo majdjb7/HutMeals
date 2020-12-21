5379a6a7432659c774f4e945472f4a9e5740980b
Majd Jaber <majdjd171@gmail.com>
2020-12-21 19:40:02 +0200

    fixed and styled buttons

--- src/components/headerComponent/header.js
+++ src/components/headerComponent/header.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react'
+import { IconContext } from 'react-icons'
+import { IoHome } from 'react-icons/io5'
+import { FaUser } from 'react-icons/fa'
+import { MdRestaurant } from 'react-icons/md'
 
 import { Navbar, Nav, NavItem, MenuItem, NavDropdown } from 'react-bootstrap';
 
@@ -11,16 +15,13 @@ class Header extends Component {
                     LOGO
                 </div>
 
-                <nav>
-                    <ul>
-                        <li>
-                            <a id="Signin" href="#">Sign In</a>
-                        </li>
-                        <li>
-                            <a id="Signup" href="#">Sign Up</a>
-                        </li>
-                    </ul>
-                </nav>
+                <button class="btn">   <MdRestaurant></MdRestaurant> Partner With Us </button>
+                
+                <button class="btn2">   <IoHome> </IoHome> Sign In </button>
+
+                <button class="btn3">   <FaUser></FaUser> Sign Up </button>
+
+        
 
             </header>
         )
