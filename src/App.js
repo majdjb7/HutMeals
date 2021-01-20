d956d2c387989fc2daf6a61d68353f12a535b34b
Majd Jaber <majdjd171@gmail.com>
2021-01-20 21:49:01 +0200

    firebase login and signup works!

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Home from './components/pages/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Business from './components/pages/Business';
 import Products from './components/pages/Products';
-import SignUp from './components/pages/SignUp';
+import Signup from './components/pages/SignUp';
+import Login from './components/pages/Login'
 
 function App() {
   return (
@@ -16,7 +17,8 @@ function App() {
           <Route path='/' exact component={Home} />
           <Route path='/business' component={Business} />
           <Route path='/products' component={Products} />
-          <Route path='/sign-up' component={SignUp} />
+          <Route path='/sign-up' component={Signup} />
+          <Route path='/login' component={Login} />
         </Switch>
       </Router>
     </>
--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -4,12 +4,11 @@ import { useAuth } from "../../AuthContext"
 import { Link, useHistory } from "react-router-dom"
 import { auth } from "../../firebase"
 
-export default function Signup() {
+export default function Login() {
   const emailRef = useRef()
   const passwordRef = useRef()
-  const passwordConfirmRef = useRef()
-  function signup(email, password) {
-    return auth.createUserWithEmailAndPassword(email, password)
+  function login(email, password) {
+    return auth.signInWithEmailAndPassword(email, password)
   }
   const [error, setError] = useState("")
   const [loading, setLoading] = useState(false)
@@ -18,18 +17,13 @@ export default function Signup() {
   async function handleSubmit(e) {
     e.preventDefault()
 
-    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-      return setError("Passwords do not match")
-    }
-
     try {
       setError("")
       setLoading(true)
-      await signup(emailRef.current.value, passwordRef.current.value)
-      alert("You have successfully created an account!")
+      await login(emailRef.current.value, passwordRef.current.value)
       history.push("/")
     } catch {
-      setError("Failed to create an account")
+      setError("Failed to log in")
     }
 
     setLoading(false)
@@ -39,7 +33,7 @@ export default function Signup() {
     <>
       <Card>
         <Card.Body>
-          <h2 className="text-center mb-4">Sign Up</h2>
+          <h2 className="text-center mb-4">Log In</h2>
           {error && <Alert variant="danger">{error}</Alert>}
           <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
@@ -50,18 +44,17 @@ export default function Signup() {
               <Form.Label>Password</Form.Label>
               <Form.Control type="password" ref={passwordRef} required />
             </Form.Group>
-            <Form.Group id="password-confirm">
-              <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control type="password" ref={passwordConfirmRef} required />
-            </Form.Group>
             <Button disabled={loading} className="w-100" type="submit">
-              Sign Up
+              Log In
             </Button>
           </Form>
+          <div className="w-100 text-center mt-3">
+            <Link to="/forgot-password">Forgot Password?</Link>
+          </div>
         </Card.Body>
       </Card>
       <div className="w-100 text-center mt-2">
-        Already have an account? <Link to="/login">Log In</Link>
+        Need an account? <Link to="/signup">Sign Up</Link>
       </div>
     </>
   )
